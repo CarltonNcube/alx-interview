fc0bdcb0be5d53c7d0ea0cbd628f45185a7bcce1
CarltonNcube <carltonncube@outlook.com>
2024-03-06 17:02:09 +0000

    a function def pascal_triangle(n): that returns a list of lists of integers representing the adds triangle of n

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
 
 def pascal_triangle(n):
-    '''
+    """
     Returns a list of lists of integers representing Pascal's triangle of n
-    '''
+    """
     if n <= 0:
         return []
 
-    tri = [[1]]
+    triangle = [[1]]
 
     for _ in range(1, n):
-        pre = tri[-1]
-        cur = [pre[0]]
+        previous_row = triangle[-1]
+        new_row = [previous_row[0]]
 
-        for i in range(1, len(pre)):
-            cur.append(pre[i - 1] + pre[i])
+        for i in range(1, len(previous_row)):
+            new_row.append(previous_row[i - 1] + previous_row[i])
 
-        cur.append(pre[-1])
-        tri.append(cur)
+        new_row.append(previous_row[-1])
+        triangle.append(new_row)
 
-    return tri
+    return triangle
