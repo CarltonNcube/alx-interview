7dd7216a25624857ab94dea9bc27789b6d90e2c0
Carlton Ncube <carltonncube@outlook.com>
2024-05-03 01:22:04 +0200

    determine the fewest number of coins needed to meet a given amount total

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,19 +1,12 @@
 #!/usr/bin/python3
 """Module for makeChange"""
 
+
 def makeChange(coins, total):
-    """Determine fewest coins needed to meet a given amount.
-
-    Args:
-        coins (list): Values of the coins in your possession
-        total (int): Total amount to meet
-
-    Returns:
-        int: Fewest number of coins needed to meet total
-    """
-    if total <= 0:
+    """Determine fewest coins needed to meet a given amount."""
+     if total <= 0:
         return 0
-    dp = [0] + [float("inf")] * total
+    dp = [0] + [float("inf")] * (total)
     for coin in coins:
         for i in range(coin, total + 1):
             dp[i] = min(dp[i], dp[i - coin] + 1)
